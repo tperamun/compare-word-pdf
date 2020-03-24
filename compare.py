d4001dd1dc1f4c4f221ed977c7a26967ca8ae32a
Timal Peramune <tperamun@uwo.ca>
2020-03-24 19:56:02 -0400

    saving

--- compare.py
+++ compare.py
@@ -1,13 +1,18 @@
 from docx2python import docx2python
 import PyPDF2
+from docx2pdf import convert
 
+convert("sample.docx", "output.pdf")
 
-word_text = []
+# word_text = []
 
 pdf_text = ""
+word_text = ""
 
-hell = docx2python('sample.docx')
-word_text= hell.text
+with open('output.pdf', mode='rb') as f:
+    reader = PyPDF2.PdfFileReader(f)
+    page = reader.getPage(0)
+    word_text+=page.extractText()
 
 with open('hehe.pdf', mode='rb') as f:
     reader = PyPDF2.PdfFileReader(f)
@@ -15,8 +20,14 @@ with open('hehe.pdf', mode='rb') as f:
     pdf_text+=page.extractText()
 
 
-print(word_text)
+# print(word_text)
 
-print("\n\n")
+# print("\n\n")
 
-print(pdf_text)
+# print(pdf_text)
+
+if word_text == pdf_text:
+    print("Same")
+
+else:
+    print("dif")
